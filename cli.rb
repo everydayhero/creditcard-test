5b54159519770d5fc0b83ba347251951c7ce4ffd
Justin Marrington <justin@marrington.net>
2016-03-30 06:34:04 +1000

    Refactor into class to get private method encapsulation

--- cli.rb
+++ cli.rb
@@ -1,7 +1,23 @@
 #!/usr/bin/ruby
 
-require './credit_card_validator'
+require_relative 'credit_card_validator'
+
+# Given a multi-line input of credit card numbers (with one card number per
+# line), this function puts a corresponding validation string for each
+def credit_card_validator(input)
+  validations = []
+  input.each_line do |line|
+    card_number = line.gsub(/\s/, '')
+    if card_number.length > 0
+      validations.push CreditCardValidation.new card_number
+    end
+  end
+  validations
+end
 
 #### Command Line Interface
 input = ARGF.read
-credit_card_validator(input)
+validations = credit_card_validator(input)
+for validation in validations
+  puts validation
+end
